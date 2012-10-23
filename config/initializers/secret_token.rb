61f2fa025b4b28b223c1f7f787b27d275bdd69f1
skaza <skaza@towson.edu>
2012-10-23 11:14:53 -0400

    skeleton project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrmHomeownerRails32::Application.config.secret_token = '7fb4b5594e41bdf4da9e067e184ac5727898d5f7a3e5ecb2c5aad90d981b0a0679eef436300656373a02f7b7e5c2d4efd56ffde1cc4befa75cf4aa75121ed674'
